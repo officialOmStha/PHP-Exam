fa7fb1e34b507631b3df2616696132fbfa09118d
Om Official <omstha71@gmail.com>
2025-12-03 23:29:32 +0545

    added about function in PHP

--- Practice.php
+++ Practice.php
@@ -27,13 +27,13 @@ if (3 > 5){
 // switch statements
 switch ($age){
     case 20 :
-        echo $name . " is 20 years old";
+        echo $name . " is 20 years old <br>";
         break;
     case 21 :
-        echo $name . " is 21 years old";
+        echo $name . " is 21 years old <br>";
         break;
     default:
-        echo $name . "'s age is unknown";
+        echo $name . "'s age is unknown <br>";
     
 }
 
@@ -42,8 +42,49 @@ for($i = 1; $i <= 5; $i++){
     echo $i . ". I am enjoying php. <br>";
 }
 
+$a = 1;
+while ($a <= 5){
+    echo $a . ", ";
+    $a++;
+}
 
+$b = 1;
 
+do{
+    echo "<br>". $b  .". I am going to study.";
+    $b++;
+}
+while($b <= 5);
+    
+//foreach loop
+
+$colors = array("red", "green", "blue", "yellow");
+$array = ["a", "b", "c", "d", "e"];
+
+foreach($colors as $color){
+    echo "<br>" . $color ;
+}
+
+echo "<br>"; 
+
+foreach($array as $c){
+    echo  $c . " , ";
+}
+
+echo "<br>";
+
+// Function in PHP
+function myMessage(){ //defining the functioning.
+    echo "This output is from a function. <br>";
+}
+
+function add($x, $y){ //defining function with parameters
+    echo "The sum of ". $x ." and ". $y . " is ". $x+$y ."<br>";
+}
+
+myMessage(); //calling a function
+add(7,8); //calling functions with parameters.
+add($age,PI);
 ?>
 </html>
 
