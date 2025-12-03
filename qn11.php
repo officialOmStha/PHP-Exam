d52b387f1e2ce4951d3709c5ee4c3c3a1627a9ab
Om Official <omstha71@gmail.com>
2025-12-03 18:11:51 +0545

    first commit

---
+++ qn11.php
@@ -0,0 +1,21 @@
+<html>
+
+<?php
+define("PI", 3.14);
+
+$age = 21;
+$greet = "Hello, I am ";
+$name = "Om Shrestha";
+
+echo ($greet . $name . " And I am ". $age . " years old. <br>");
+var_dump($age);
+var_dump($name);
+echo "<br>". PI;
+echo "<br>". pi();
+
+
+
+
+?>
+</html>
+
