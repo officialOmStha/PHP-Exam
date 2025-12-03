64d408cf3b8f2f856721373da147bff355fca3c6
Om Official <omstha71@gmail.com>
2025-12-03 18:33:46 +0545

    added more contents

--- Practice.php
+++ Practice.php
@@ -14,6 +14,36 @@ var_dump(PI); // can be used for constants too.
 echo "<br>". PI; // using <br> in echo for line breaking.
 echo "<br>". pi(); // using inbuilt php function to get value of PI.
 
+// if statements
+
+if(5 > 3) {
+    echo "<br>I am the best. <br>";
+}
+
+if (3 > 5){
+    echo "<br>I am the worst. <br>";
+}
+
+// switch statements
+switch ($age){
+    case 20 :
+        echo $name . " is 20 years old";
+        break;
+    case 21 :
+        echo $name . " is 21 years old";
+        break;
+    default:
+        echo $name . "'s age is unknown";
+    
+}
+
+// Loops in php
+for($i = 1; $i <= 5; $i++){
+    echo $i . ". I am enjoying php. <br>";
+}
+
+
+
 ?>
 </html>
 
