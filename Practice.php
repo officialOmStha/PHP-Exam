bb2dbe8aae343015182f912f11aa6e8037cd3c25
Om Official <omstha71@gmail.com>
2025-12-03 18:21:04 +0545

    making notes using comments

--- Practice.php
+++ Practice.php
@@ -1,17 +1,18 @@
 <html>
 
 <?php
-define("PI", 3.14);
+define("PI", 3.14); // this is one way to define a constant datatype is float.
 
-$age = 21;
-$greet = "Hello, I am ";
-$name = "Om Shrestha";
+$age = 21; //this is a variable with integer datatype
+$greet = "Hello, I am "; //this is a variable with string datatype
+$name = "Om Shrestha"; //this is a variable with string datatype
 
-echo ($greet . $name . " And I am ". $age . " years old. <br>");
-var_dump($age);
-var_dump($name);
-echo "<br>". PI;
-echo "<br>". pi();
+echo ($greet . $name . " And I am ". $age . " years old. <br>"); //echo is used to display result.
+var_dump($age); // var_dump gives us datatype and (value), 
+var_dump($name); // var_dump gives us datatype, (length) and value. length for stings only.
+var_dump(PI); // can be used for constants too.
+echo "<br>". PI; // using <br> in echo for line breaking.
+echo "<br>". pi(); // using inbuilt php function to get value of PI.
 
 ?>
 </html>
